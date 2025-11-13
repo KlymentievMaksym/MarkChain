c7c52cec8856c33c05288510384d26f75c5ade20
Dadmon <113892002+KlymentievMaksym@users.noreply.github.com>
2025-11-13 15:43:31 +0200

    ?

--- MCMC/task_2.py
+++ MCMC/task_2.py
@@ -48,7 +48,6 @@ def receive_array_counts(counter: collections.Counter, learn_characters: dict):
     for bigram in counter_keys:
         M[learn_characters[bigram[0]], learn_characters[bigram[1]]] = counter[bigram]
 
-
     M += 1
     for index in range(M.shape[0]):
         M[index] = M[index] / M[index].sum()
@@ -56,23 +55,14 @@ def receive_array_counts(counter: collections.Counter, learn_characters: dict):
 
     return M
 
-# @numba.jit()
-def score(f: np.ndarray, M: np.array, ciphered_bigrams: str, learn_characters: dict):
+@numba.jit()
+def score(f: np.ndarray, M: np.array, text_indices: np.ndarray) -> float:
     score = 0
-    # k = 0
-    for bigram in ciphered_bigrams:
-        scor = M[f[learn_characters[bigram[0]]], f[learn_characters[bigram[1]]]]
-        score += scor
-    return score
+    for index in range(len(text_indices) - 1):
+        score += M[f[text_indices[index]], f[text_indices[index+1]]]
+    return score #/ len(text_indices)
 
-# def swap(f: dict):
-#     f_ = f.copy()
-#     f_keys = list(f.keys())
-#     i, j = np.random.choice(len(f), 2, replace=False)
-#     k1, k2 = f_keys[i], f_keys[j]
-#     f_[k1], f_[k2] = f_[k2], f_[k1]
-#     return f_
-
+@numba.jit()
 def swap(f: np.ndarray) -> np.ndarray:
     f_ = f.copy()
     i, j = np.random.choice(len(f), 2, replace=False)
@@ -80,7 +70,8 @@ def swap(f: np.ndarray) -> np.ndarray:
     return f_
 
 
-def task_2(path_to_learn: str, path_to_decipher: str, iterations: int = 10, every: int = 1):
+def task_2(path_to_learn: str, path_to_decipher: str, iterations: int = 10, every: int = 1, T = 1):
+    # n = 100000
 
     text_to_learn = load_text(path_to_learn)
     text_to_deciphr = load_text(path_to_decipher)
@@ -92,7 +83,8 @@ def task_2(path_to_learn: str, path_to_decipher: str, iterations: int = 10, ever
     learn_characters = {ch: i for i, ch in enumerate(learn_unique_character)}
 
     learn_bigrams = get_bigrams(text_to_learn)
-    decipher_bigrams = get_bigrams(text_to_decipher)
+    # decipher_bigrams = get_bigrams(text_to_decipher)
+    text_indices = np.array([learn_characters[ch] for ch in text_to_decipher], dtype=int)  #[:n]
 
     counter = collections.Counter(learn_bigrams)
     M = receive_array_counts(counter, learn_characters)
@@ -101,43 +93,42 @@ def task_2(path_to_learn: str, path_to_decipher: str, iterations: int = 10, ever
     # f = dict(zip(alphabet, alphabet))
 
     best_f = f.copy()
-    best_score = score(f, M, decipher_bigrams, learn_characters)
+    best_score = score(f, M, text_indices)
 
-    for iter in tqdm.tqdm(range(iterations)):
-        f_score = score(f, M, decipher_bigrams, learn_characters)
+    f_score = best_score
+    for iteration in tqdm.tqdm(range(iterations)):
         f_ = swap(f)
-        f_score_ = score(f_, M, decipher_bigrams, learn_characters)
-
-        # print(f"[Score 1] {f_score_}")
-        # print(f"[Score 2] {f_score}")
-        accept = f_score_/f_score #if f_score != 0 else 0.5
-        # print(accept)
+        f_score_ = score(f_, M, text_indices)
+        accept = f_score_ / f_score
+        # if accept > 0 or np.random.rand() < np.exp(accept / T):
         if np.random.rand() < accept:
-            f = f_
-        # print(f)
-        new_score = score(f, M, decipher_bigrams, learn_characters)
-        if best_score < new_score:
-            best_score = new_score
-            best_f = f.copy()
+            f, f_score = f_, f_score_
+        if f_score > best_score:
+            best_score, best_f = f_score, f.copy()
 
-        if (iter + 1) % every == 0:
-            # print(f"[F] {f}")
-            # print(f"[Best F] {best_f}")
-            # print(f"[New score] Iter {iter}: {new_score}")
-            print(f"[Best score] Iter {iter + 1}: {best_score}")
+        # T *= 0.999
+
+        if (iteration + 1) % every == 0:
+            print(f"[Best score] Iter {iteration + 1}: {best_score}")
 
 
+    # F = {ch: learn_unique_character[f[i]] for i, ch in enumerate(learn_unique_character)}
+    # best_F = {ch: learn_unique_character[best_f[i]] for i, ch in enumerate(learn_unique_character)}
     F = {str(ls): str(ls_) for ls, ls_ in zip(learn_unique_character, learn_unique_character[f])}
     best_F = {str(ls): str(ls_) for ls, ls_ in zip(learn_unique_character, learn_unique_character[best_f])}
-    print(f"[F] {F}")
+
+    n = 100
+
+    # print(f"[F] {F}")
     print(f"[Best F] {best_F}")
     print(f"[Best score] {best_score}")
-    n = 300
-    print(f"[F] {cipher(text_to_decipher_r[:n], f)}")
-    print(f"[Best F] {cipher(text_to_decipher_r[:n], best_f)}")
+    # print(f"[F] {cipher(text_to_decipher_r[:n], f)}")
+    decode_map = {learn_unique_character[f[i]]: learn_unique_character[i] for i in range(len(best_f))}
+    print(f"[Ciphered] {text_to_decipher_r[:n]}\n")
+    print(f"[Decipher] {cipher(text_to_decipher_r[:n], best_F)}\n")
     print(f"[Real text] {text_to_deciphr[:n]}")
 
 
 
 if __name__ == '__main__':
-    task_2("./Data/TheWarOfTheWorlds.txt", "./Data/TheTimeMachine.txt", 10000, every = 1000)
+    task_2("./Data/TheWarOfTheWorlds.txt", "./Data/TheTimeMachine.txt", 1_000_000, every = 250_000)
